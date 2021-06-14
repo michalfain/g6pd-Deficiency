d7fcbbb890721b3f4f967f2a22a5288b75797923
michal fain <michelle.fain@gmail.com>
2021-06-14 15:35:14 +0300

    ContactUs screen logic done

--- app/src/main/java/com/example/g6pd/ContactUs.java
+++ app/src/main/java/com/example/g6pd/ContactUs.java
@@ -1,8 +1,13 @@
 package com.example.g6pd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +15,9 @@ public class ContactUs extends AppCompatActivity {
 
     EditText etName, etEmail, etMessage;
     Button btnSubmit;
+    String email = "michelle.fain@gmail.com" ;
+    static final String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+↵\n" +
+            ")*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,50 @@ public class ContactUs extends AppCompatActivity {
         etEmail = findViewById(R.id.et_email);
         etMessage = findViewById(R.id.et_message);
         btnSubmit = findViewById(R.id.btn_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkName() && checkEmail() &&
+                        !etMessage.getText().toString().isEmpty()) {
+                    sendMail();
+                    Toast.makeText(ContactUs.this, "Thank you for your feedback!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(ContactUs.this, "Please enter all fields!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    void sendMail(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        intent.putExtra(Intent.EXTRA_SUBJECT, etName.getText().toString().trim());
+        intent.putExtra(Intent.EXTRA_TEXT, etMessage.getText().toString().trim());
+        intent.setType("message/rfc822");
+        startActivity(intent);
+        etName.setText("");
+        etEmail.setText("");
+        etMessage.setText("");
+    }
+    public boolean checkName(){
+        boolean isChecked;
+        if (etName.getText().toString().length() < 2){
+            isChecked = false;
+        }else {
+            isChecked = true;
+        }
+        return isChecked;
+    }
+    public boolean checkEmail(){
+        boolean isChecked;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(etEmail.getText().toString());
+        if (matcher.matches()){
+            isChecked = false;
+        }else {
+            isChecked = true;
+        }
+        return isChecked;
     }
 }
