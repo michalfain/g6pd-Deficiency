02dfeb463760c789c9792be2ca83e10c7cc66f45
michal fain <michelle.fain@gmail.com>
2021-08-09 15:38:29 +0300

    about us ui logic

--- app/src/main/java/com/example/g6pd/AboutUs.java
+++ app/src/main/java/com/example/g6pd/AboutUs.java
@@ -1,14 +1,53 @@
 package com.example.g6pd;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUs extends AppCompatActivity {
+    TextView tvRacheli, tvRinat, tvPressHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_about_us);
+        tvRacheli = findViewById(R.id.racheli_email);
+        tvRinat = findViewById(R.id.rinat_email);
+        tvPressHere = findViewById(R.id.press_here);
+        tvRacheli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail(Constants.racheliEmail);
+            }
+        });
+        tvRinat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail(Constants.rinatEmail);
+            }
+        });
+        tvPressHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLink(Constants.fbLink);
+            }
+        });
+
+    }
+    private void goToLink(String link){
+        Uri uri = Uri.parse(link);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+
+    }
+    void sendMail(String email){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        intent.setType("message/rfc822");
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/g6pd/Constants.java
+++ app/src/main/java/com/example/g6pd/Constants.java
@@ -9,7 +9,9 @@ public static String food = "food";
 public static String type = "type";
 public static String addInfo = "addInfo";
 public static String michalEmail = "michelle.fain@gmail.com";
-public static String thankYouForFeedback = "תודה רבה ל המשוב!";
+public static String racheliEmail = "Rachel7386@gmail.com";
+public static String rinatEmail = "g6pdteam@gmail.com";
+public static String thankYouForFeedback = "תודה רבה על המשוב!";
 public static String pleaseFillAllFields = "נא למלא את כל השדות!";
 public static String alleries = "Alleries";
 public static String commonInfo = "commonInfo";
@@ -21,6 +23,8 @@ public static String g6pd = "g6pd";
 public static String noPhoto = "no photo";
 public static String search = "חיפוש";
 public static String commonQA = "commonQA";
+public static String fbLink = "https://www.facebook.com/%D7%9E%D7%98%D7%94-%D7%94%D7%9E%D7%95%D7%93%D7%A2%D7%95%D7%AA-%D7%9C%D7%9E%D7%A1%D7%95%D7%9B%D7%A0%D7%95%D7%AA-%D7%9C%D7%A4%D7%95%D7%9C-110211037924021";
 static final String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+↵\n" +
             ")*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+
 }
